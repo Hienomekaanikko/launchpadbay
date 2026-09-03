a154b0c51ef2fae8e85b7c767ac6cbe934d7fe1c
Mikko Suokas <mikesuokas@gmail.com>
2026-09-03 19:50:58 +0300

    Basic ui for the landingpage to get going

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,15 +11,46 @@ function LobbyView() {
   )
 }
 
-function LoginView({ onClose, setLoggedIn }) {
+function ValidateRegister( {checkValidated, setRegisterView} ) {
   return (
-    <div className="LoginView">
-      <button onClick={onClose}>x</button>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button onClick={()=> {setLoggedIn(true); onClose()}}>Login</button>
-      <button>Not registered?</button>
+    <div className="RegisterView">
+      Input/registering validation not done yet but this would say "Registering succesful! Or Failed"
+      <button onClick={()=> {checkValidated(false); setRegisterView(false)}}>Continue!</button>
     </div>
+  )
+}
+
+function RegisterView({ setRegisterView }) {
+  const [validated, checkValidated] = useState(false)
+  return (
+    <>
+      <div className="RegisterView">
+        <button onClick={() => setRegisterView(false)}>x</button>
+        <input type="text" placeholder="Set username"/>
+        <input type="password" placeholder="Set password"/>
+        <input type="password" placeholder="Type password again"/>
+        <button onClick={() => checkValidated(true)}>Submit</button> 
+      </div>
+      {validated && <ValidateRegister checkValidated={checkValidated} setRegisterView={setRegisterView}/>}
+
+    </>
+  )
+}
+
+function LoginView({ setLoggedIn, setShowLogin }) {
+  const [registerView, setRegisterView] = useState(false)
+
+  return (
+    <>
+      <div className="LoginView">
+        <button onClick={() => setShowLogin(false)}>x</button>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button onClick={()=> {setLoggedIn(true); setShowLogin(false)}}>Login</button>
+        <button onClick={()=> {setRegisterView(true)}}>Not registered yet?</button>
+      </div>
+       {registerView && <RegisterView setRegisterView={setRegisterView} />}
+    </>
   )
 }
 
@@ -40,8 +71,8 @@ function App() {
       <main>
           {showLogin && 
           <LoginView 
-            onClose={() => setShowLogin(false)}
             setLoggedIn={setLoggedIn}
+            setShowLogin={setShowLogin}
             />}
           {loggedIn && <button 
                         className="Lobbybutton"
