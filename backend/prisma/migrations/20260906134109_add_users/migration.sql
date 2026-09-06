a20e152b7424c644d3e37a69deeb08a55b9100a7
Mikko Suokas <mikesuokas@gmail.com>
2026-09-06 16:52:16 +0300

    adding/migrating user table to prisma

---
+++ backend/prisma/migrations/20260906134109_add_users/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(50) NOT NULL,
+    `username` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password_hash` VARCHAR(255) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `users_username_key`(`username`),
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
